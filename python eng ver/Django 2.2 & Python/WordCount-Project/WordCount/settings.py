e9172719003c8c4a370fc5ac5385be2668a4981e
sebinha <victorfcardoso@hotmail.com>
2021-07-07 03:37:40 -0300

    Django 3.2.5

--- python eng ver/Django 2.2 & Python/WordCount-Project/WordCount/settings.py
+++ python eng ver/Django 2.2 & Python/WordCount-Project/WordCount/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--vpk#+__k2w#c!@*j30syczj^1r3bl+b92nqsmu%6#xm29i5an'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
