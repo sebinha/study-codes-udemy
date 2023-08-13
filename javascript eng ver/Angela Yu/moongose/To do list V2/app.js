4c83da96f8a655033c0d8cb8291de93f6994578e
sebinha <victorfcardoso@hotmail.com>
2023-08-13 20:18:42 -0300

    fix: vercel not applying styles

--- javascript eng ver/Angela Yu/moongose/To do list V2/app.js
+++ javascript eng ver/Angela Yu/moongose/To do list V2/app.js
@@ -19,7 +19,6 @@ app.set("views", path.join(__dirname, "views"));
 
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-app.use(express.static(__dirname + "/public/"));
 
 mongoose
   .connect(process.env.MONGODB_URI)
